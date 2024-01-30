59af8159650b1daa9c196a8ff2131c7c64e6d818
minhoJJJ <sucessmh25@naver.com>
2024-01-30 14:57:13 +0900

    스프링 부트 1.2ver

--- src/main/java/com/myOrder/repositories/MemberRepository.java
+++ src/main/java/com/myOrder/repositories/MemberRepository.java
@@ -1,10 +1,17 @@
 package com.myOrder.repositories;
 
 import com.myOrder.entity.Member;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
     Member findByUserId(String userId);
+
+
 
 }
