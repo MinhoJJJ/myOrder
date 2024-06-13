729fc6e176fe695bfaa31d25a9ad32011d711ad7
minhoJJJ <sucessmh25@naver.com>
2024-06-13 10:16:30 +0900

    세터 어노테이션 추가

--- src/main/java/com/myOrder/dto/memberDto.java
+++ src/main/java/com/myOrder/dto/memberDto.java
@@ -1,40 +1,39 @@
 package com.myOrder.dto;
 import java.time.LocalDate;
 import lombok.Getter;
+import lombok.Setter;
 
 public class memberDto {
 
     @Getter
+    @Setter
     private String id;
     @Getter
+    @Setter
     private String password;
     @Getter
+    @Setter
     private String name;
     @Getter
+    @Setter
     private String auth;
     @Getter
+    @Setter
     private LocalDate joinDate;
     @Getter
+    @Setter
     private String lang;
     @Getter
+    @Setter
     private LocalDate birthday;
     @Getter
+    @Setter
     private String email;
     @Getter
+    @Setter
     private LocalDate upt_dt;
     @Getter
+    @Setter
     private String currency;
-
-    public void setId(String id) {this.id = id;}
-    public void setPassword(String password) {this.password = password;}
-    public void setName(String name) {this.name = name;}
-    public void setAuth(String auth) {this.auth = auth;}
-    public void setJoinDate(LocalDate joinDate) {this.joinDate = joinDate;}
-    public void setLang(String lang) {this.lang = lang;}
-    public void setBirthday(LocalDate birthday) { this.birthday = birthday;}
-    public void setEmail(String email) { this.email = email;}
-    public void setUpt_dt(LocalDate upt_dt) { this.upt_dt = upt_dt; }
-    public void setCurrency(String currency) { this.currency = currency;}
-
 
 }
--- src/main/java/com/myOrder/entity/Member.java
+++ src/main/java/com/myOrder/entity/Member.java
@@ -14,56 +14,53 @@ public class Member {
 
     @Id
     @Getter
+    @Setter
     @Column(name = "ID")
     private String id;
 
     @Getter
+    @Setter
     @Column(name = "PASSWORD")
     private String password;
 
     @Getter
+    @Setter
     @Column(name = "NAME")
     private String name;
 
     @Getter
+    @Setter
     @Column(name = "AUTH")
     private String auth;
 
     @Getter
+    @Setter
     @Column(name = "JOINDATE")
     private LocalDate joinDate;
 
     @Getter
+    @Setter
     @Column(name = "LANG")
     private String lang;
 
     @Getter
+    @Setter
     @Column(name = "UPT_DT")
     private LocalDate upt_dt;
 
     @Getter
+    @Setter
     @Column(name = "BIRTHDAY")
     private LocalDate birthday;
 
     @Getter
+    @Setter
     @Column(name = "EMAIL")
     private String email;
 
     @Getter
+    @Setter
     @Column(name = "CURRENCY")
     private String currency;
-
-    public void setId(String id) {this.id = id;}
-    public void setPassword(String password) {this.password = password;}
-    public void setName(String name) {this.name = name;}
-    public void setAuth(String auth) {this.auth = auth;}
-    public void setJoinDate(LocalDate joinDate) {this.joinDate = joinDate;}
-    public void setLang(String lang) {this.lang = lang;}
-    public void setUpt_dt(LocalDate upt_dt) { this.upt_dt = upt_dt;}
-    public void setBirthday(LocalDate birthday) {this.birthday = birthday;}
-    public void setEmail(String email) {this.email = email;}
-    public void setCurrency(String currency) {this.currency = currency;}
-
-
 
 }
