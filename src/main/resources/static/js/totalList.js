2f88cabe1b51d057865c6866fe9d875511646966
minhoJJJ <sucessmh25@naver.com>
2024-07-11 14:03:19 +0900

    전체내역 UI 수정

--- src/main/resources/static/js/totalList.js
+++ src/main/resources/static/js/totalList.js
@@ -1,25 +1,3 @@
 $(document).ready(function() {
-    var gridOptions = {
-        columnDefs: [
-            { headerName: "지출/수입", field: "type" },
-            { headerName: "결제 종류", field: "paymentMethod" },
-            { headerName: "카테고리", field: "category" },
-            { headerName: "연월일", field: "date" },
-            { headerName: "내용", field: "description" },
-            { headerName: "금액", field: "amount" }
-        ],
-        defaultColDef: {
-            flex: 1,
-            minWidth: 100,
-            sortable: true,
-            resizable: true,
-        }
-    };
 
-    // ag-Grid 초기화
-    var myGrid = document.querySelector('#myGrid');
-    new agGrid.Grid(myGrid, gridOptions);
-
-    // 여기에 데이터를 가져와서 gridOptions.api.setRowData()를 사용하여 데이터를 추가합니다.
-    // 예: gridOptions.api.setRowData(data);
 });
